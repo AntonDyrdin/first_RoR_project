9178f3c65d15ada8ae791b5af240afe2164b4899
AntonDyrdin <anton_dyrdin@mail.ru>
2019-10-12 00:51:17 +0300

    get 'api/last_todo'

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -11,4 +11,9 @@ class ApiController < ApplicationController
   	todo = Todo.find(params[:id])
     render json: todo
   end
+
+  def get_last_todo
+  	todo = Todo.last(1)
+    render json: todo
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'api/projects'
   get 'api/todos'
   get 'api/todo:id', action: :todo, controller: 'api'
+  get 'api/last_todo', action: :get_last_todo, controller: 'api'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
